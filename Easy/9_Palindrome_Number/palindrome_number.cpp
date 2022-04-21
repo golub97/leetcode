6a43aa2fca82313cc85264881fd5ea7f952f625f
golub97 <48694046+golub97@users.noreply.github.com>
2022-04-21 20:24:23 +0200

    Brute force (naive) algorithm.

--- Easy/9_Palindrome_Number/palindrome_number.cpp
+++ Easy/9_Palindrome_Number/palindrome_number.cpp
@@ -4,10 +4,10 @@
 
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        if((x < 0) || (((x % 10) == 0) && (x != 0))) {
+    bool isPalindrome(int x) 
+    {
+        if((x < 0) || (((x % 10) == 0) && (x != 0))) 
             return false;
-        }
 
         int temp = x;
         long revertedNumber = 0;
