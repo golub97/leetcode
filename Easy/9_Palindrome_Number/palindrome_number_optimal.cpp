e9c68de03eacfc5e6f510235c147db8b08f7b91a
golub97 <48694046+golub97@users.noreply.github.com>
2022-04-21 20:23:48 +0200

    Optimal solution.

--- Easy/9_Palindrome_Number/palindrome_number_optimal.cpp
+++ Easy/9_Palindrome_Number/palindrome_number_optimal.cpp
@@ -4,9 +4,9 @@
 
 class Solution {
 public:
-    bool isPalindrome(int x) {
-
-    string temp = to_string(x);
-    return temp == string(temp.rbegin(), temp.rend());
+    bool isPalindrome(int x) 
+    {
+        string temp = to_string(x);
+        return temp == string(temp.rbegin(), temp.rend());
     }
 };
